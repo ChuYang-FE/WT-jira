2c9c9de9dde46caec1cd2d678f6536e3f15f0c09
William Tan <865428527@qq.com>
2021-12-14 10:45:15 +0800

    feat: custom http

---
+++ src/utils/http.ts
@@ -0,0 +1,45 @@
+import * as auth from 'authProvider';
+import qs from 'qs';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+interface CustomConfig extends RequestInit {
+  data?: object;
+  token?: string;
+}
+
+export const http = (
+  endpoint: string,
+  { data, token, headers, ...customConfig }: CustomConfig,
+) => {
+  const config = {
+    method: 'GET',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+      'Content-Type': data ? 'application/json' : '',
+    },
+    ...customConfig,
+  };
+
+  if (config.method.toUpperCase() === 'GET') {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+
+  return window
+    .fetch(`${apiUrl}/${endpoint}`, config)
+    .then(async (response) => {
+      if (response.status === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({ message: '请重新登录' });
+      }
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
+};
